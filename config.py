b9d67bf37d7e2276c8efb0462d0276ec58c620d9
mmtg445 <mmtg445@gmail.com>
2024-11-06 15:06:20 +0600

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://rm-movie-flix.onrender.com")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
